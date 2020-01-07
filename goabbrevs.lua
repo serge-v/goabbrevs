0e587e986bf496d395a242790160aa4705946d96
Serge Voilokov <serge0x76@gmail.com>
2020-01-06 23:07:51 -0500

    Add plugin

---
+++ goabbrevs.lua
@@ -0,0 +1,34 @@
+-- go abbreviations plugin
+-- triggers by space or '='.
+-- see gen.go for abbreviations list.
+
+abbrevs = loadfile("abbrevs.lua")
+abbrevs()
+
+local str = ""
+
+function replace(bp, repl, back)
+	bp:SelectWordLeft()
+	bp.Cursor:DeleteSelection()
+	bp.Buf:Insert(-bp.Cursor.Loc, repl)
+	loc = bp.Cursor:Move(back, bp.Buf)
+	bp.Cursor:GotoLoc(loc)
+end
+ 
+function onRune(bp, r)
+	local ft = bp.Buf:FileType()
+	if ft ~= "go" then
+		return true
+	end
+
+	if r == " " or r == "=" then
+		local repl, back = expand(str)
+		if repl ~= "" then
+			replace(bp, repl, back)
+		end
+		str = ""
+		return true	
+	end
+	str = str .. r
+	return true
+end
